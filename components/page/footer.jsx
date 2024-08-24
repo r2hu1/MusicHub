386f884f2ee90d456402d0d3942b21c6699a223d
r2hu1 <hi@rahul.eu.org>
2024-08-24 16:53:33 +0530

    new footer texts

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer className="py-5 mt-8 text-center">
-            <p className="text-sm">Made with ❤️ by <a className="hover:underline hover:text-primary" href="https://github.com/r2hu1">r2hu1</a><span className="text-primary">.</span></p>
-            <ul className="flex gap-1 items-center justify-center mt-1">
-                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-primary-foreground rounded-full p-1 px-2" href="https://instagram.com/r.rah_ul" target="_blank">Instagram</a></li>
-                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-primary-foreground rounded-full p-1 px-2" href="https://github.com/r2hu1" target="_blank">Github</a></li>
-            </ul>
+        <footer className="py-5 mt-8 px-6 md:px-20 lg:px-32">
+            <div>
+                <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
+                <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" href="https://github.com/r2hu1">r2hu1</a> hosted on vercel.</p>
+            </div>
+            <p className="text-muted-foreground text-sm mt-2 max-w-lg">I dont own the api it belongs to its respective owners, built for educational purposes.</p>
+            <div className="flex gap-3 mt-3">
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/r2hu1/musichub">Source Code</Link>
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://rahul.eu.org">Portfolio</Link>
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/r.rah_ul">Instagram</Link>
+            </div>
         </footer>
     )
 }
