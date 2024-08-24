969466164a2576630c1f4dd0bf7eb8afa16de2ec
Rahul Rajput <newasram86@gmail.com>
2024-08-24 20:17:35 +0530

    Update header.jsx

--- components/page/header.jsx
+++ components/page/header.jsx
@@ -8,7 +8,7 @@ import { buttonVariants } from "../ui/button";
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center py-5 md:px-20 lg:px-28 px-6">
+        <header className="flex justify-between items-center py-5 md:px-20 lg:px-32 px-6">
             <Logo />
             <div className="flex items-center justify-center gap-2">
                 <div className="hidden md:block">
