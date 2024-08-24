5a055d9fb4d54d1795be05f2f79329f689725721
r2hu1 <hi@rahul.eu.org>
2024-08-24 19:46:54 +0530

    fix pwa and scrollbr

--- app/(root)/album/_components/Album.jsx
+++ app/(root)/album/_components/Album.jsx
@@ -2,10 +2,9 @@
 
 import SongCard from "@/components/cards/song";
 import { Badge } from "@/components/ui/badge";
-import { ScrollBar } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getAlbumById } from "@/lib/fetch";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { useEffect, useState } from "react"
 
 export default function Album({ id }) {
@@ -26,15 +25,17 @@ export default function Album({ id }) {
         <main className="px-6 md:px-20 lg:px-32 py-5">
             {data.image ? (
                 <div>
-                    <img src={data.image[2].url} alt={data.name} className="w-full md:w-[200px] md:h-[200px] rounded-2xl" />
-                    <div className="mt-5 mb-1">
-                        <h1 className="text-xl font-medium">{data.name}</h1>
-                        <p className="text-sm text-muted-foreground mt-1">{data.description}</p>
-                        <p className="text-sm text-muted-foreground">by <span className="text-primary">{data.artists.primary.map(artist => artist.name).join(",")}</span></p>
+                    <div className="md:flex gap-10">
+                        <img src={data.image[2].url} alt={data.name} className="w-full md:w-[200px] md:h-[200px] rounded-2xl" />
+                        <div className="mt-5 mb-1">
+                            <h1 className="text-xl font-medium">{data.name}</h1>
+                            <p className="text-sm text-muted-foreground mt-1">{data.description}</p>
+                            <p className="text-sm text-muted-foreground mb-1">by <span className="text-primary">{data.artists.primary.map(artist => artist.name).join(",")}</span></p>
+                            <Badge>
+                                {data.songCount} songs
+                            </Badge>
+                        </div>
                     </div>
-                    <Badge>
-                        {data.songCount} songs
-                    </Badge>
                     <div className="mt-12">
                         <ScrollArea className="rounded-md mt-4">
                             <div className="flex gap-3">
@@ -49,11 +50,27 @@ export default function Album({ id }) {
             ) : (
                 <>
                     <Skeleton className="md:h-[200px] md:w-[200px] rounded-2xl w-full h-[400px]" />
-                    <Skeleton className="h-3 mt-4 w-32"/>
-                    <Skeleton className="h-3 mt-2 mb-3 w-40"/>
-                    <Skeleton className="h-3 mt-1 mb-8 w-14"/>
+                    <Skeleton className="h-4 mt-4 w-32" />
+                    <Skeleton className="h-3 mt-3 mb-1.5 w-40" />
+                    <Skeleton className="h-3 w-14" />
+                    <Skeleton className="h-6 mt-2 mb-8 w-20" />
                     <ScrollArea className="rounded-md mt-4">
                         <div className="flex gap-6">
+                            <div className="grid gap-2">
+                                <Skeleton className="h-[200px] w-[200px]" />
+                                <Skeleton className="h-4 w-28" />
+                                <Skeleton className="h-3 w-20 -mt-1" />
+                            </div>
+                            <div className="grid gap-2">
+                                <Skeleton className="h-[200px] w-[200px]" />
+                                <Skeleton className="h-4 w-28" />
+                                <Skeleton className="h-3 w-20 -mt-1" />
+                            </div>
+                            <div className="grid gap-2">
+                                <Skeleton className="h-[200px] w-[200px]" />
+                                <Skeleton className="h-4 w-28" />
+                                <Skeleton className="h-3 w-20 -mt-1" />
+                            </div>
                             <div className="grid gap-2">
                                 <Skeleton className="h-[200px] w-[200px]" />
                                 <Skeleton className="h-4 w-28" />
