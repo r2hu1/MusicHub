27eaa2e3e83095927a443879f0f59c1a139bd144
r2hu1 <hi@rahul.eu.org>
2024-08-26 07:21:52 -0700

    new artist card

--- components/cards/artist.jsx
+++ components/cards/artist.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function ArtistCard({ image, name, id }) {
     return (
-        <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`}>
-            <div>
-                <img src={image} alt={name} className="rounded-2xl h-[100px] min-w-[100px] object-cover"/>
+        <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`} className="border border-border rounded-md bg-secondary/30">
+            <div className="p-2 overflow-hidden">
+                <img src={image} alt={name} className="hover:scale-105 transition cursor-pointer rounded-md h-[100px] min-w-[100px] object-cover"/>
             </div>
-            <div className="mt-2">
-                <h1 className="text-sm">{name.split(" ")[0] || null} {name.split(" ")[1] || null}</h1>
+            <div className="p-2 -mt-1">
+                <h1 className="text-sm max-w-[100px] text-ellipsis text-nowrap overflow-hidden">{name.split(" ")[0] || null} {name.split(" ")[1] || null}</h1>
             </div>
         </Link>
     )
