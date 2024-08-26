79bf228dbc8cf93f47df471da3ff2b5d4036d971
Rahul Rajput <newasram86@gmail.com>
2024-08-26 19:59:53 +0530

    Update artist.jsx

--- components/cards/artist.jsx
+++ components/cards/artist.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ArtistCard({ image, name, id }) {
     return (
         <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`} className="border border-border rounded-md bg-secondary/30">
-            <div className="p-2 overflow-hidden">
+            <div className="p-2 overflow-hidden h-[100px] w-[100px]">
                 <img src={image} alt={name} className="hover:scale-105 transition cursor-pointer rounded-md h-[100px] min-w-[100px] object-cover"/>
             </div>
             <div className="p-2 -mt-1">
