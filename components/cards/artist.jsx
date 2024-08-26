86d5d6bd9f88d37db44c33aa6a791e07c4a1c081
Rahul Rajput <newasram86@gmail.com>
2024-08-26 20:03:03 +0530

    Update artist.jsx

--- components/cards/artist.jsx
+++ components/cards/artist.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function ArtistCard({ image, name, id }) {
     return (
-        <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`} className="border border-border rounded-md bg-secondary/30">
-            <div className="p-2 overflow-hidden h-[100px] w-[100px] rounded-md">
+        <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`} className="border border-border rounded-md bg-secondary/30 p-2">
+            <div className="overflow-hidden h-[100px] w-[100px] rounded-md">
                 <img src={image} alt={name} className="hover:scale-105 transition cursor-pointer rounded-md h-[100px] min-w-[100px] object-cover"/>
             </div>
-            <div className="p-2 -mt-1">
+            <div className="mt-1">
                 <h1 className="text-sm max-w-[100px] text-ellipsis text-nowrap overflow-hidden">{name.split(" ")[0] || null} {name.split(" ")[1] || null}</h1>
             </div>
         </Link>
