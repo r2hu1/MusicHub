d862a795983b1d78eff0e8868bea2b86069445ab
r2hu1 <hi@rahul.eu.org>
2024-08-27 21:25:57 +0530

    fix

--- app/(player)/[id]/page.jsx
+++ app/(player)/[id]/page.jsx
@@ -7,7 +7,7 @@ export const generateMetadata = async ({ params }) => {
     const title = await getSongsById(params.id);
     const data = await title.json();
     return {
-        title: `Now Playing - ${data.song}`
+        title: `Now Playing - ${data.name}`
     }
 }
 
