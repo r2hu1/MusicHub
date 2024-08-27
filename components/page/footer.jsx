9debe56508c62fefbe3c151755d1a0c1558fe377
r2hu1 <hi@rahul.eu.org>
2024-08-26 23:29:45 -0700

    dn

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
                 <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
                 <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" href="https://github.com/r2hu1">r2hu1</a> hosted on vercel.</p>
             </div>
-            <p className="text-muted-foreground text-sm mt-2 max-w-lg">I dont own the api it belongs to its respective owners, built for educational purposes.</p>
+            <p className="text-muted-foreground text-sm mt-2 max-w-lg">We don't own any of the songs and images it belongs to its respective owners, built for educational purposes.</p>
             <div className="flex gap-3 mt-3">
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/r2hu1/musichub">Source Code</Link>
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://rahul.eu.org">Portfolio</Link>
