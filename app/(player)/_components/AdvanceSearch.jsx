11c16650406f223baa1a9ecb8af411a9c0b38468
Rahul Rajput <newasram86@gmail.com>
2024-08-27 22:10:22 +0530

    Update AdvanceSearch.jsx

--- app/(player)/_components/AdvanceSearch.jsx
+++ app/(player)/_components/AdvanceSearch.jsx
@@ -75,10 +75,14 @@ export default function AdvanceSearch() {
                     <CredenzaHeader>
                         <CredenzaTitle className="text-left flex gap-2">
                             <Input autoFocus={query == ""} value={query} onChange={(e) => setQuery(e.target.value)} className="w-full" type="search" name="query" placeholder="Search for songs by name..." autoComplete="off" />
-                            <Button className="min-w-10" size="icon" asChild>
-                                <Link href={`/search/${query}`} disabled={query == ""}>
+                            <Button className="min-w-10" size="icon" asChild={query != ""}>
+                                {query != "" ? (
+                                    <Link href={`/search/${query}`}>
                                      <Search className="h-4 w-4" />
-                                 </Link>
+                                    </Link>
+                                    ) : (
+                                     <Search className="h-4 w-4" />
+                                 )}
                             </Button>
                         </CredenzaTitle>
                     </CredenzaHeader>
