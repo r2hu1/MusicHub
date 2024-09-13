4c567d3f58a1b87c1c3030ecef2dc2a072e90b8e
r2hu1 <hi@rahul.eu.org>
2024-09-13 19:30:20 +0530

    last paayed

--- components/music-provider.jsx
+++ components/music-provider.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { MusicContext } from "@/hooks/use-context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Player from "./cards/player";
 import { useSearchParams } from "next/navigation";
 
@@ -8,6 +8,11 @@ export default function MusicProvider({ children }) {
     const params = useSearchParams().get("playing");
     const [music, setMusic] = useState(params ? params : null);
 
+    useEffect(() => {
+        if(localStorage.getItem("last-played")) {
+            setMusic(localStorage.getItem("last-played"))
+        }
+    }, []);
     return (
         <MusicContext.Provider value={{ music, setMusic }}>
             {children}
