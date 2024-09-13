8ec221961732d8f79f1f5d740b65d799ba48058d
Rahul Rajput <newasram86@gmail.com>
2024-09-13 19:35:45 +0530

    Update music-provider.jsx

--- components/music-provider.jsx
+++ components/music-provider.jsx
@@ -5,13 +5,14 @@ import Player from "./cards/player";
 import { useSearchParams } from "next/navigation";
 
 export default function MusicProvider({ children }) {
-    const params = useSearchParams().get("playing");
-    const [music, setMusic] = useState(params ? params : null);
+    const [music, setMusic] = useState(null);
 
     useEffect(() => {
         if(localStorage.getItem("last-played")) {
             setMusic(localStorage.getItem("last-played"))
         }
+        const params = useSearchParams().get("playing");
+        setMusic(params ? params : null);
     }, []);
     return (
         <MusicContext.Provider value={{ music, setMusic }}>
