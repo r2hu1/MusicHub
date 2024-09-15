b768e9ffdd04eca67d3b267c89e5c5d100d39377
r2hu1 <hi@rahul.eu.org>
2024-09-15 12:32:51 +0530

    dn

--- components/cards/player.jsx
+++ components/cards/player.jsx
@@ -120,7 +120,7 @@ export default function Player() {
                             <Button size="icon" className="p-0 h-8 w-8" variant={!isLooping ? "outline" : "secondary"} onClick={loopSong}>
                                 {!isLooping ? <Repeat className="h-3.5 w-3.5" /> : <Repeat1 className="h-3.5 w-3.5" />}
                             </Button>
-                            <Button size="icon" className="p-0 h-8 w-8" onClick={() => { values.setMusic(null); localStorage.clear(); }}>
+                            <Button size="icon" className="p-0 h-8 w-8" onClick={() => { values.setMusic(null); localStorage.clear(); audioRef.current.currentTime = 0; }}>
                                 <X className="h-3.5 w-3.5" />
                             </Button>
                         </div>
