23e173803661007ec1b0622fef05ec1e56bcd633
r2hu1 <hi@rahul.eu.org>
2024-11-10 12:18:58 +0530

    changed border and btn bg

--- components/page/search.jsx
+++ components/page/search.jsx
@@ -23,8 +23,8 @@ export default function Search() {
         <>
         <Link href={"/search/" + query} ref={linkRef}></Link>
             <form onSubmit={handleSubmit} className="flex items-center">
-                <Input ref={inpRef} value={query} onChange={(e) => setQuery(e.target.value)} autoComplete="off" className="w-full !outline-none !ring-0 md:w-[300px] border-r-0 rounded-r-none" type="search" name="query" placeholder="Type something to search..." />
-                <Button type="submit" size="icon" className="min-w-10 rounded-l-none"><SearchIcon className="w-4 h-4"/></Button>
+                <Input ref={inpRef} value={query} onChange={(e) => setQuery(e.target.value)} autoComplete="off" className="w-full !outline-none !ring-0 md:w-[300px] border-r-0 rounded-2xl rounded-r-none" type="search" name="query" placeholder="Type something to search..." />
+                <Button variant="outline" type="submit" size="icon" className="min-w-10 border-l-0 rounded-2xl rounded-l-none"><SearchIcon className="w-4 h-4"/></Button>
             </form>
         </>
     )
