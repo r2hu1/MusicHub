ea6ca46eb27d78e9408e6d78c6099ee6b8ad7361
r2hu1 <hi@rahul.eu.org>
2024-11-10 12:20:03 +0530

    cg: dropdown

--- components/ModeToggle.jsx
+++ components/ModeToggle.jsx
@@ -5,14 +5,36 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme, resolvedTheme: theme, systemTheme } = useTheme();
+  const { setTheme } = useTheme()
 
   return (
-    <div className="flex rounded-lg overflow-hidden border border-border">
-      <Button onClick={() => setTheme("dark")} size="icon" className="rounded-none dark:bg-primary dark:text-primary-foreground" variant={"ghost"}><Moon className="h-4 w-4" /></Button>
-      <Button onClick={() => setTheme("light")} size="icon" className="rounded-none dark:bg-transparent hover:bg-primary dark:text-foreground bg-primary hover:text-primary-foreground text-primary-foreground" variant={"ghost"}><Sun className="h-4 w-4" /></Button>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="default" className="rounded-full h-[42px] w-[42px]" size="icon">
+          <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
