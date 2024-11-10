31941dc7c5e779c7ef73b03e65ee234d1e47a360
r2hu1 <hi@rahul.eu.org>
2024-11-10 12:19:48 +0530

    cg: gap

--- app/(root)/search/_components/Search.jsx
+++ app/(root)/search/_components/Search.jsx
@@ -34,11 +34,11 @@ export default function Search({ params }) {
         <div className="py-12 -mt-9 px-6 md:px-20 lg:px-32">
             <div className="grid gap-4">
                 <div className="mt-2">
-                    <h1 className="text-base font-medium">Search Results</h1>
+                    <h1 className="text-base">Search Results</h1>
                     <p className="text-xs text-muted-foreground">search results for "{decodeURI(query)}"</p>
                 </div>
                 <ScrollArea>
-                    <div className="flex gap-3">
+                    <div className="flex gap-6">
                         {songs.length ? songs.map((song) => (
                             <SongCard key={song.id} id={song.id} image={song.image[2].url} artist={song.artists.primary[0]?.name || "unknown"} title={song.name} />
                         )) : (
@@ -54,12 +54,12 @@ export default function Search({ params }) {
                     <ScrollBar orientation="horizontal" className="hidden" />
                 </ScrollArea>
 
-                <div className="mt-4">
-                    <h1 className="font-medium text-base">Related Albums</h1>
+                <div className="mt-8">
+                    <h1 className="text-base">Related Albums</h1>
                     <p className="text-xs text-muted-foreground">Albums related to "{decodeURI(query)}"</p>
                 </div>
                 <ScrollArea className="whitespace-nowrap pb-4">
-                    <div className="flex gap-3">
+                    <div className="flex gap-6">
                         {albums.length ? albums.map((song) => (
                             <AlbumCard key={song.id} lang={song.language} desc={song.description || null} id={`album/${song.id}`} image={song.image[2].url} title={song.name} artist={song.artists.primary[0]?.name || "unknown"} />
                         )) : (
@@ -75,13 +75,13 @@ export default function Search({ params }) {
                     <ScrollBar orientation="horizontal" className="hidden" />
                 </ScrollArea>
 
-                <div className="mt-1">
+                <div className="mt-4">
                     <h1 className="text-base font-medium">Related Artists</h1>
                     <p className="text-xs text-muted-foreground">artists related to "{decodeURI(query)}"</p>
                 </div>
                 <ScrollArea>
                     {artists.length > 0 ? (
-                        <div className="flex gap-3">
+                        <div className="flex gap-5">
                             {[...new Set(artists.map(a => a.artists.primary[0]?.id))].map(id => (
                                 <ArtistCard key={id} id={id} image={artists.find(a => a.artists.primary[0]?.id === id).artists.primary[0]?.image[2]?.url || `https://az-avatar.vercel.app/api/avatar/?bgColor=0f0f0f0&fontSize=60&text=${artists.find(a => a.artists.primary[0]?.id === id).artists.primary[0]?.name.split("")[0].toUpperCase() || "U"}`} name={artists.find(a => a.artists.primary[0]?.id === id).artists?.primary[0]?.name || "unknown"} />
                             ))}
