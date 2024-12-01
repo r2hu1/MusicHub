ddfc937888f0aabf5900b3f4340b811b69bea48c
r2hu1 <hi@rahul.eu.org>
2024-12-01 13:04:26 +0530

    changed header [added: share/back]

--- components/page/header.jsx
+++ components/page/header.jsx
@@ -1,24 +1,27 @@
-import { Bookmark, Star } from "lucide-react";
-import Link from "next/link";
+"use client"
 import { ModeToggle } from "../ModeToggle";
 import Logo from "./logo";
-import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 import Search from "./search";
-import { buttonVariants } from "../ui/button";
+import { ChevronDown, ChevronLeft, Share2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Header() {
+    const path = usePathname();
+    console.log(path);
     return (
-        <header className="flex justify-between items-center py-5 md:px-20 lg:px-32 px-6">
-            <Logo />
-            <div className="flex items-center justify-center gap-2">
-                <div className="hidden md:block">
-                    <Search />
-                </div>
-                {/* <Link href="/saved" className={cn(buttonVariants({ variant: "default" }), "gap-2")}>
-                    Saved <Bookmark className="w-4 h-4" />
-                </Link> */}
+        <header className="grid gap-3 pt-5 px-5 pb-5 md:px-20 lg:px-32">
+            <div className="flex items-center justify-between">
+                {path == "/" ? (
+                    <Button size="icon" onClick={() => { navigator.share({ url: window.location.href }) }} variant="outline" className="rounded-full lg:-ml-4"><Share2 className="w-4 h-4" /></Button>
+                ) : (
+                    <Button size="icon" asChild variant="outline" className="rounded-full"><Link href="/"><ChevronLeft className="w-5 h-5" /></Link></Button>
+                )}
+                <Logo />
                 <ModeToggle />
             </div>
+            <Search />
         </header>
     )
 }
