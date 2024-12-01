2815f51a6a87825969535c13ef3636af7ae2aadd
r2hu1 <hi@rahul.eu.org>
2024-12-01 13:05:22 +0530

    radius

--- components/cards/artist.jsx
+++ components/cards/artist.jsx
@@ -4,9 +4,9 @@ export default function ArtistCard({ image, name, id }) {
     return (
         <Link href={"/search/" + `${encodeURI(name.toLowerCase().split(" ").join("+"))}`}>
             <div className="overflow-hidden h-[100px] w-[100px] rounded-md">
-                <img src={image} alt={name} className="hover:scale-105 transition cursor-pointer rounded-md h-[100px] min-w-[100px] object-cover"/>
+                <img src={image} alt={name} className="hover:scale-105 transition cursor-pointer rounded-full h-[100px] min-w-[100px] object-cover"/>
             </div>
-            <div className="mt-2">
+            <div className="mt-2 text-center">
                 <h1 className="text-sm max-w-[100px] text-ellipsis text-nowrap overflow-hidden">{name.split(" ")[0] || null} {name.split(" ")[1] || null}</h1>
             </div>
         </Link>
