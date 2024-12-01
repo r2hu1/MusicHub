5b29d2bcb3ac723b42250fb8a55c5e21fbc72524
r2hu1 <hi@rahul.eu.org>
2024-12-01 13:04:52 +0530

    rm srch

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -8,12 +8,9 @@ export default function RootLayout({ children }) {
     return (
         <main>
             <Header />
-            <div className="md:hidden block px-6 mb-2">
-                <Search />
-            </div>
             <MusicProvider>
                 {children}
-                <Player/>
+                <Player />
             </MusicProvider>
             <Footer />
         </main>
