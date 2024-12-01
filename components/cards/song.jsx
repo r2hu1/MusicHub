92c32d7239d5cc363bfc6643f861d4b1f0899481
r2hu1 <hi@rahul.eu.org>
2024-12-01 13:05:33 +0530

    sm chng

--- components/cards/song.jsx
+++ components/cards/song.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Skeleton } from "../ui/skeleton";
 import { useContext, useEffect } from "react";
 import { MusicContext } from "@/hooks/use-context";
+import { IoPlay } from "react-icons/io5";
 
 export default function SongCard({ title, image, artist, id, desc }) {
     const ids = useContext(MusicContext);
@@ -14,8 +15,9 @@ export default function SongCard({ title, image, artist, id, desc }) {
         <div className="h-fit w-[200px]">
             <div className="overflow-hidden rounded-md">
                 {image ? (
-                    <div onClick={() => { ids.setMusic(id); setLastPlayed(); }}>
-                        <img src={image} alt={title} className="h-[182px] blurz w-full bg-secondary/60 rounded-md transition hover:scale-105 cursor-pointer" />
+                    <div className="relative" onClick={() => { ids.setMusic(id); setLastPlayed(); }}>
+                        <img src={image} alt={title} className="h-[182px] blurz w-full bg-secondary/60 rounded-md transition hover:scale-105 cursor-context-menu" />
+                        <div className="cursor-pointer absolute z-10 bottom-2 left-2 bg-background/60 backdrop-blur-md rounded-full h-8 w-8 flex items-center justify-center"><IoPlay className="w-4 h-4 -mr-0.5 dark:fill-white"/></div>
                     </div>
                 ) : (
                     <Skeleton className="w-full h-[182px]" />
@@ -23,7 +25,7 @@ export default function SongCard({ title, image, artist, id, desc }) {
             </div>
             <div className="cursor-pointer">
                 {title ? (
-                    <div onClick={() => { ids.setMusic(id); setLastPlayed(); }} className="mt-2 flex items-center justify-between">
+                    <div onClick={() => { ids.setMusic(id); setLastPlayed(); }} className="mt-3 flex items-center justify-between">
                         <h1 className="text-base">{title.slice(0, 20)}{title.length > 20 && '...'}</h1>
                     </div>
                 ) : (
@@ -33,7 +35,7 @@ export default function SongCard({ title, image, artist, id, desc }) {
                     <p className="text-xs text-muted-foreground">{desc.slice(0, 30)}</p>
                 )}
                 {artist ? (
-                    <p className="text-xs text-muted-foreground">by <span className="text-primary">{artist.slice(0, 20)}{artist.length > 20 && '...'}</span></p>
+                    <p className="text-sm font-light text-muted-foreground">{artist.slice(0, 20)}{artist.length > 20 && '...'}</p>
                 ) : (
                     <Skeleton className="w-10 h-2 mt-2" />
                 )}
--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="py-5 backdrop-blur-3xl mt-8 px-6 md:px-20 lg:px-32">
-            <div>
+            {/* <div>
                 <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
-                <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" href="https://github.com/r2hu1">r2hu1</a> hosted on vercel.</p>
-            </div>
-            <p className="text-muted-foreground text-sm mt-2 max-w-lg">We don't own any of the songs and images it belongs to its respective owners, built for educational purposes.</p>
-            <div className="flex gap-3 mt-3">
+            </div> */}
+            <p className="text-center text-muted-foreground text-sm mt-2 max-w-lg font-light">Built for educational purpose.</p>
+            <p className="text-center text-sm text-muted-foreground">Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://github.com/r2hu1">r2hu1</a>.</p>
+            <div className="flex gap-3 items-center justify-center mt-3">
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/r2hu1/musichub">Source Code</Link>
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://rahul.eu.org">Portfolio</Link>
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/r.rah_ul">Instagram</Link>
