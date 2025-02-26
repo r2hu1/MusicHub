03dee317355e99f7fd081ff3a6b606df0693bcb1
r2hu1 <hi@rahul.eu.org>
2025-02-26 20:17:33 +0530

    changed ui for mobile

--- app/(player)/_components/Player.jsx
+++ app/(player)/_components/Player.jsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import { NextContext } from "@/hooks/use-context";
 import Next from "@/components/cards/next";
 import { useMusic } from "@/components/music-provider";
+import { IoPause } from "react-icons/io5";
 
 export default function Player({ id }) {
     const [data, setData] = useState([]);
@@ -78,11 +79,6 @@ export default function Player({ id }) {
     const loopSong = () => {
         audioRef.current.loop = !audioRef.current.loop;
         setIsLooping(!isLooping);
-        if (isLooping) {
-            toast.success('Removed from Loop!');
-        } else {
-            toast.success('Added to Loop!');
-        }
     };
 
     const handleShare = () => {
@@ -131,7 +127,7 @@ export default function Player({ id }) {
         return handleRedirect();
     }, [currentTime, duration, isLooping, next?.nextData?.id]);
     return (
-        <div className="mb-3 mt-4">
+        <div className="mb-3 mt-14">
             <audio onPlay={() => setPlaying(true)} onPause={() => setPlaying(false)} onLoadedData={() => setDuration(audioRef.current.duration)} autoPlay={playing} src={audioURL} ref={audioRef}></audio>
             <div className="grid gap-6 w-full">
                 <div className="sm:flex px-6 md:px-20 lg:px-32 grid gap-5 w-full">
@@ -140,7 +136,7 @@ export default function Player({ id }) {
                             <Skeleton className="md:w-[130px] aspect-square rounded-2xl md:h-[150px]" />
                         ) : (
                             <div className="relative">
-                                <img src={data.image[2].url} className="sm:h-[150px] h-full bg-secondary/50 rounded-2xl sm:w-[200px] w-full object-cover" />
+                                <img src={data.image[2].url} className="sm:h-[150px] h-full aspect-square bg-secondary/50 rounded-2xl sm:w-[200px] w-[250px] sm:mx-0 mx-auto object-cover" />
                                 <img src={data.image[2].url} className="hidden dark:block absolute top-0 left-0 w-[110%] h-[110%] blur-3xl -z-10 opacity-50" />
                             </div>
                         )}
@@ -166,26 +162,26 @@ export default function Player({ id }) {
                         </div>
                     ) : (
                         <div className="flex flex-col justify-between w-full">
-                            <div>
-                                <h1 className="text-xl font-bold md:max-w-lg max-w-[260px]">{data.name}</h1>
-                                <p className="text-xs text-muted-foreground">by <Link href={"/search/" + `${encodeURI(data.artists.primary[0].name.toLowerCase().split(" ").join("+"))}`} className="text-foreground">{data.artists.primary[0]?.name || "unknown"}</Link></p>
+                            <div className="text-center sm:text-left sm:mt-0 mt-3">
+                                <h1 className="text-xl font-bold md:max-w-lg">{data.name}</h1>
+                                <p className="text-sm text-muted-foreground">by <Link href={"/search/" + `${encodeURI(data.artists.primary[0].name.toLowerCase().split(" ").join("+"))}`} className="text-foreground">{data.artists.primary[0]?.name || "unknown"}</Link></p>
                             </div>
-                            <div className="grid gap-2 w-full mt-5 sm:mt-0">
+                            <div className="grid gap-2 w-full mt-8 sm:mt-0 px-2 sm:px-0">
                                 <Slider onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full" />
                                 <div className="w-full flex items-center justify-between">
-                                    <span className="text-xs">{formatTime(currentTime)}</span>
-                                    <span className="text-xs">{formatTime(duration)}</span>
+                                    <span className="text-sm">{formatTime(currentTime)}</span>
+                                    <span className="text-sm">{formatTime(duration)}</span>
                                 </div>
-                                <div className="flex items-center justify-between mt-2">
-                                    <div className="flex items-center gap-3 sm:mt-0">
+                                <div className="flex items-center justify-between sm:mt-2">
+                                    <div className="flex items-center justify-center sm:justify-normal w-full gap-3 sm:mt-0">
                                         <Button size="icon" variant="outline" onClick={loopSong}>
                                             {!isLooping ? <Repeat className="h-4 w-4" /> : <Repeat1 className="h-4 w-4" />}
                                         </Button>
-                                        <Button size="icon" variant={playing ? "gooeyRight" : "gooeyLeft"} onClick={togglePlayPause}>
+                                        <Button size="icon" variant={playing ? "default" : "secondary"} className="h-14 sm:h-10 sm:w-10 w-14" onClick={togglePlayPause}>
                                             {playing ? (
-                                                <Pause className="h-4 w-4" />
+                                                <IoPause className="h-6 sm:h-4 sm:w-4 w-6" />
                                             ) : (
-                                                <Play className="h-4 w-4" />
+                                                <Play className="h-6 sm:h-4 sm:w-4 w-6" />
                                             )}
                                         </Button>
                                         <Button size="icon" variant="outline" onClick={downloadSong}>
@@ -196,7 +192,7 @@ export default function Player({ id }) {
                                             )}
                                         </Button>
                                     </div>
-                                    <Button size="icon" variant="outline" onClick={handleShare}><Share2 className="h-4 w-4" /></Button>
+                                    {/* <Button size="icon" variant="ghost" onClick={handleShare}><Share2 className="h-4 w-4" /></Button> */}
                                 </div>
                             </div>
                         </div>
