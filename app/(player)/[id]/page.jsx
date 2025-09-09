ab309307567d75cfcec3aa344e5f51eede441a6a
Rahul Rajput <newasram86@gmail.com>
2025-09-09 21:03:12 +0530

    feat: change rec lok

--- app/(player)/[id]/page.jsx
+++ app/(player)/[id]/page.jsx
@@ -5,45 +5,46 @@ import AdvanceSearch from "../_components/AdvanceSearch";
 import Search from "@/components/page/search";
 
 export const generateMetadata = async ({ params }) => {
-    const title = await getSongsById(params.id);
-    const data = await title.json();
-    const song = data?.data[0];
-    return {
-        title: song.name,
-        description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"} from the album "${song.album?.name}".`,
-        openGraph: {
-            title: song.name,
-            description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"}.`,
-            type: "music.song",
-            url: song.url,
-            images: [
-                {
-                    url: song.image[2]?.url || song.image[1]?.url || song.image[0]?.url,
-                    width: 1200,
-                    height: 630,
-                    alt: song.name,
-                },
-            ],
-            music: {
-                album: song.album?.url,
-                release_date: song.releaseDate,
-                musician: data?.artists?.primary[0]?.name || "unknown",
-            },
+  const title = await getSongsById(params.id);
+  const data = await title.json();
+  console.log(data);
+  const song = data?.data[0];
+  return {
+    title: song.name,
+    description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"} from the album "${song.album?.name}".`,
+    openGraph: {
+      title: song.name,
+      description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"}.`,
+      type: "music.song",
+      url: song.url,
+      images: [
+        {
+          url: song.image[2]?.url || song.image[1]?.url || song.image[0]?.url,
+          width: 1200,
+          height: 630,
+          alt: song.name,
         },
-        twitter: {
-            card: "summary_large_image",
-            title: song.name,
-            description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"}.`,
-            images: song.image?.[0]?.url,
-        },
-    };
-}
+      ],
+      music: {
+        album: song.album?.url,
+        release_date: song.releaseDate,
+        musician: data?.artists?.primary[0]?.name || "unknown",
+      },
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: song.name,
+      description: `Listen to "${song.name}" by ${data?.artists?.primary[0]?.name || "unknown"}.`,
+      images: song.image?.[0]?.url,
+    },
+  };
+};
 
 export default function Page({ params }) {
-    return (
-        <div>
-            <Player id={params.id} />
-            <Recomandation id={params.id} />
-        </div>
-    )
+  return (
+    <div>
+      <Player id={params.id} />
+      <Recomandation id={params.id} />
+    </div>
+  );
 }
--- components/cards/next.jsx
+++ components/cards/next.jsx
@@ -3,24 +3,25 @@ import { Badge } from "../ui/badge";
 import { Play } from "lucide-react";
 
 export default function Next({ name, artist, image, id, next = true }) {
-    return (
-        <Link href={`/${id}`}>
-            <div className="flex items-center gap-3 bg-secondary p-2 rounded-md">
-                <img src={image} className="aspect-square w-10 rounded-md" />
-                <div className="overflow-hidden flex-1">
-                    <h1 className="text-secondary-foreground text-base text-ellipsis whitespace-nowrap overflow-hidden sm:max-w-md max-w-[150px]">
-                        {name}
-                    </h1>
-                    <p className="-mt-0.5 mb-1 text-xs text-muted-foreground overflow-hidden text-ellipsis whitespace-nowrap">
-                        by{' '}
-                        <span className="text-secondary-foreground">
-                            {artist}
-                        </span>
-                    </p>
-                </div>
-                {next && <Badge className="!font-normal">next</Badge>}
-                {!next && <Badge><Play size={16} className="w-3 px-0 h-4" /></Badge>}
-            </div>
-        </Link>
-    )
+  return (
+    <Link href={`/${id}`}>
+      <div className="flex items-center gap-3 bg-secondary/30 border p-2 rounded-md">
+        <img src={image} className="aspect-square w-10 rounded-md" />
+        <div className="overflow-hidden flex-1">
+          <h1 className="text-secondary-foreground text-base text-ellipsis whitespace-nowrap overflow-hidden sm:max-w-md max-w-[150px]">
+            {name}
+          </h1>
+          <p className="-mt-0.5 mb-1 text-xs text-muted-foreground overflow-hidden text-ellipsis whitespace-nowrap">
+            by <span className="text-secondary-foreground">{artist}</span>
+          </p>
+        </div>
+        {next && <Badge className="!font-normal">next</Badge>}
+        {!next && (
+          <Badge>
+            <Play size={16} className="w-3 px-0 h-4" />
+          </Badge>
+        )}
+      </div>
+    </Link>
+  );
 }
