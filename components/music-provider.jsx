16989d46133fb79fa9a3ba3cc9b9ca6e24a50137
Rahul Rajput <newasram86@gmail.com>
2025-09-14 23:36:16 +0530

    feat: added down pro

--- components/music-provider.jsx
+++ components/music-provider.jsx
@@ -1,22 +1,31 @@
-"use client"
+"use client";
 import { MusicContext } from "@/hooks/use-context";
 import { useContext, useEffect, useState } from "react";
 import Player from "./cards/player";
 
 export default function MusicProvider({ children }) {
-    const [music, setMusic] = useState(null);
-    const [current, setCurrent] = useState(null);
+  const [music, setMusic] = useState(null);
+  const [current, setCurrent] = useState(null);
+  const [downloadProgress, setDownloadProgress] = useState(0);
 
-    useEffect(() => {
-        if (localStorage.getItem("last-played")) {
-            setMusic(localStorage.getItem("last-played"));
-        }
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem("last-played")) {
+      setMusic(localStorage.getItem("last-played"));
+    }
+  }, []);
 
-    return (
-        <MusicContext.Provider value={{ music, setMusic, current, setCurrent }}>
-            {children}
-        </MusicContext.Provider>
-    )
+  return (
+    <MusicContext.Provider
+      value={{
+        music,
+        setMusic,
+        current,
+        setCurrent,
+        downloadProgress,
+        setDownloadProgress,
+      }}
+    >
+      {children}
+    </MusicContext.Provider>
+  );
 }
-export const useMusic = () => useContext(MusicContext);
